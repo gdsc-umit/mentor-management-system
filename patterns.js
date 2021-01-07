15d6590d30a30f5df3e6c80310cb22b9c6b7b8fd
OMKAR AGRAWAL <Omkaragrawal@users.noreply.github.com>
2021-01-07 17:18:27 +0530

    adding pattern.js

---
+++ patterns.js
@@ -0,0 +1,12 @@
+/** 
+ * Write down all the patterns here compulsorily
+ * The patterns in to backend are for the frontend team, the request is needs to be in the same pattern as given here
+ * The patterns in to frontend are for the backend  team, the request is needs to be in the same pattern as given here
+ * 
+*/
+
+
+// to Backend
+
+
+// to Frontend
